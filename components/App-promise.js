0851758d6a9e0fd010b0f016dc76931a27682eb6
Bartek <bartekma89@gmail.com>
2018-02-07 15:11:13 +0100

    added Promise in ajax

--- components/App-promise.js
+++ components/App-promise.js
@@ -3,69 +3,80 @@ var GIPHY_API_URL = 'https://api.giphy.com';
 
 App = React.createClass({
 
-	getInitialState: function() {
-		return {
-			loading: false,
-			searchingText: '',
-			gif: {}
-		}
-	},
+    getInitialState: function() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        }
+    },
 
-	handleSearch: function(searchingText) {
-		this.setState({
-			loading: true
-		});
-		this.getGif(searchingText, function(gif) {
-			this.setState({
-				loading: false,
-				gif: gif,
-				searchingText: searchingText
-			})
-		}.bind(this));
-	},
+    handleSearch: function(searchingText) {
+        this.setState({
+            loading: true
+        });
+        this.getGif(searchingText)
+            .then(responseGif => {
+                var data = JSON.parse(responseGif).data;
+                this.setState({
+                    loading: false,
+                    gif: {
+                        url: data.fixed_width_downsampled_url,
+                        sourceURL: data.url
+                    }
+                });
+            })
+            .catch(error => console.log(`Something went wrong: ${error}`));
 
-	getGif: function(searchingText, callback) {
-		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
-		var xhr = new XMLHttpRequest();
-		xhr.open('GET', url);
+    },
 
-			xhr.onload = function() {
+    getGif: function(searchingText, callback) {
+        var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
 
-			if(xhr.status === 200) {
-				var data = JSON.parse(xhr.responseText).data;
-				var gif = {
-					url: data.fixed_width_downsampled_url,
-					sourceURL: data.url
-				};
-				callback(gif)
-			}
-		}
+        const promise = new Promise(function(resolve, reject) {
 
-		xhr.send();
-	},
+            xhr.onload = function() {
 
-	render: function() {
-		var url = 'http://giphy.com';
+                if (xhr.status === 200) {
+                    resolve(xhr.responseText)
+                } else {
+                    reject(new Error(xhr.statusCode))
+                }
+            };
 
-		var styles = {
-			textAlign: 'center',
-			margin: '0 auto',
-			width: '90%'
-		}
+            xhr.onerror = function() {
+                reject(new Error(xhr.statusCode));
+            }
 
-		return (
-				<div style={styles}>
-					<h1>Wyszukiwarka GIFów</h1>
-					<p>Znajdz gifa na <a href={url}>giphy</a>. Nacisnij enter, aby pobrac kolejne gify</p>
-					<Search 
-						onSearch={this.handleSearch}
-					/>
-					<Gif 
-						loading = {this.state.loading}
-						url = {this.state.gif.url}
-						sourceUrl = {this.state.gif.sourceURL}
-					/>
-				</div>
-			)
-	}
+        })
+
+        xhr.send();
+
+        return promise;
+    },
+
+    render: function() {
+        var url = 'http://giphy.com';
+
+        var styles = {
+            textAlign: 'center',
+            margin: '0 auto',
+            width: '90%'
+        }
+
+        return ( <
+            div style = { styles } >
+            <
+            h1 > Wyszukiwarka GIFów < /h1> <
+            p > Znajdz gifa na < a href = { url } > giphy < /a>. Nacisnij enter, aby pobrac kolejne gify</p >
+            <
+            Search onSearch = { this.handleSearch }
+            /> <
+            Gif loading = { this.state.loading } url = { this.state.gif.url } sourceUrl = { this.state.gif.sourceURL }
+            /> < /
+            div >
+        )
+    }
 })
